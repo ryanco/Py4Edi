a8d66361fd2a2ab95d0e4961cf3ab6f1dc3901b4
ryanco <ryangcolwell@gmail.com>
2013-03-02 18:31:47 -0600

    Added validation for group count within the interchange. Added some missing comments. Reduced usage of the EdiValidator class.

--- Configuration.py
+++ Configuration.py
@@ -1,17 +1,12 @@
-from Settings import CurrentSettings
-
 
 class Configuration(object):
     pass
 
 
 class EdiDocumentConfiguration(Configuration):
-    version = CurrentSettings.version
-    element_separator = CurrentSettings.element_separator
-    segment_terminator = CurrentSettings.segment_terminator
-    sub_element_separator = CurrentSettings.sub_element_separator
 
     def __init__(self, version, element_separator, segment_terminator, sub_element_separator):
+        """Creates a new Edi Document Configuration"""
         Configuration.__init__(self)
         self.version = version
         self.element_separator = element_separator
